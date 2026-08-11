a8603ccd373e10badc1ddeed8d1039ac8277a75a
rizkianandageo <rizkiananda.geo@gmail.com>
2026-08-11 13:10:15 +0700

    Fix iOS standalone PWA config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   appleWebApp: {
     capable: true,
-    statusBarStyle: "default",
+    statusBarStyle: "black-translucent",
     title: "ECERIES",
   },
   formatDetection: {
